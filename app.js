8600f22f80d990bed7ade54acec985d5ab042dc1
Non T <117605943+Ratanon691@users.noreply.github.com>
2024-06-25 12:13:13 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-// write code here
+let username = prompt(`Username`)
+let email = prompt(`email`)
+let password = prompt(`password`)
+
+let user ={
+  username: username ,
+  email: email,
+  password: password
+}
